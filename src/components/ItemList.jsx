7b24a4ff4c9f673d6e19f2f92f7a3fba481fe2cf
Will Price <pricew86@gmail.com>
2023-12-26 14:26:11 +0800

    add context provider and consumer

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,57 @@
 import EmptyView from './EmptyView';
+import Select from 'react-select';
+import { useItemsContext } from '../lib/hooks';
+import { useMemo, useState } from 'react';
 
-export default function ItemList({
-  items,
-  handleDeleteItem,
-  handleToggleItem,
-}) {
+const sortingOptions = [
+  {
+    label: 'Sort by default',
+    value: 'default',
+  },
+  {
+    label: 'Sort by packed',
+    value: 'packed',
+  },
+  {
+    label: 'Sort by unpacked',
+    value: 'unpacked',
+  },
+];
+
+export default function ItemList() {
+  const [sortBy, setSortBy] = useState('default');
+  const { items, handleDeleteItem, handleToggleItem } = useItemsContext();
+
+  //spread the items into a new array in order to avoid mutating the original array
+  const sortedItems = useMemo(
+    () =>
+      [...items].sort((a, b) => {
+        if (sortBy === 'packed') {
+          return b.packed - a.packed;
+        }
+        if (sortBy === 'unpacked') {
+          return a.packed - b.packed;
+        }
+        return;
+      }),
+    [items, sortBy]
+  );
+
   return (
     <ul className='item-list'>
       {items.length === 0 ? <EmptyView /> : null}
-      {items.map((item) => (
+      {items.length > 0 ? (
+        <section className='sorting'>
+          <Select
+            onChange={(option) => {
+              setSortBy(option.value);
+            }}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      ) : null}
+      {sortedItems.map((item) => (
         <Item
           item={item}
           key={item.id}
